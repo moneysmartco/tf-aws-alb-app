0dda1c10a19d315554ee393430c04f59703c148f
Tung Nguyen <tungns.inf@gmail.com>
2018-02-04 17:58:15 +0800

    Try to separate the logic

--- listener-rule-http.tf
+++ listener-rule-http.tf
@@ -2,13 +2,13 @@
 # Domain Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "domain_http" {
-  count        = "${var.setup_listener_rule ? length(var.domains) : 0}"
+  count        = "${var.setup_listener_rule && var.setup_target_group ? length(var.domains) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.domain_priority_init + count.index}"
 
   action {
     type             = "forward"
-    target_group_arn = "${var.setup_target_group ? aws_alb_target_group.app.arn : var.app_target_group_arn}"
+    target_group_arn = "${aws_alb_target_group.app.arn}"
   }
 
   condition {
@@ -21,17 +21,36 @@ resource "aws_alb_listener_rule" "domain_http" {
   }
 }
 
+resource "aws_alb_listener_rule" "domain_http" {
+  count        = "${var.setup_listener_rule && var.setup_target_group == false ? length(var.domains) : 0}"
+  listener_arn = "${var.alb_listener_http_arn}"
+  priority     = "${var.domain_priority_init + count.index}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${var.app_target_group_arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${element(var.domains, count.index)}"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 #----------------------------------------
 # Domain & URL Mixed Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "domain_and_url_http" {
-  count        = "${var.setup_listener_rule ? length(var.domains_and_urls) : 0}"
+  count        = "${var.setup_listener_rule && var.setup_target_group ? length(var.domains_and_urls) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.domain_and_url_priority_init + count.index}"
 
   action {
     type             = "forward"
-    target_group_arn = "${var.setup_target_group ? aws_alb_target_group.app.arn : var.app_target_group_arn}"
+    target_group_arn = "${aws_alb_target_group.app.arn}"
   }
 
   condition {
@@ -49,17 +68,61 @@ resource "aws_alb_listener_rule" "domain_and_url_http" {
   }
 }
 
+resource "aws_alb_listener_rule" "domain_and_url_http" {
+  count        = "${var.setup_listener_rule && var.setup_target_group == false ? length(var.domains_and_urls) : 0}"
+  listener_arn = "${var.alb_listener_http_arn}"
+  priority     = "${var.domain_and_url_priority_init + count.index}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${var.app_target_group_arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${element(keys(var.domains_and_urls), count.index)}"]
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["${element(values(var.domains_and_urls), count.index)}"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 #----------------------------------------
 # URL Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "url_http" {
-  count        = "${var.setup_listener_rule ? length(var.urls) : 0}"
+  count        = "${var.setup_listener_rule && var.setup_target_group ? length(var.urls) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.url_priority_init + count.index}"
 
   action {
     type             = "forward"
-    target_group_arn = "${var.setup_target_group ? aws_alb_target_group.app.arn : var.app_target_group_arn}"
+    target_group_arn = "${aws_alb_target_group.app.arn}"
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["${element(var.urls, count.index)}"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_alb_listener_rule" "url_http" {
+  count        = "${var.setup_listener_rule && var.setup_target_group == false ? length(var.urls) : 0}"
+  listener_arn = "${var.alb_listener_http_arn}"
+  priority     = "${var.url_priority_init + count.index}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${var.app_target_group_arn}"
   }
 
   condition {
--- listener-rule-https.tf
+++ listener-rule-https.tf
@@ -2,7 +2,27 @@
 # Domain Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "domain_https" {
-  count        = "${var.setup_listener_rule ? length(var.domains) : 0}"
+  count        = "${var.setup_listener_rule && var.setup_target_group ? length(var.domains) : 0}"
+  listener_arn = "${var.alb_listener_https_arn}"
+  priority     = "${var.domain_priority_init + count.index}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_alb_target_group.app.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${element(var.domains, count.index)}"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_alb_listener_rule" "domain_https" {
+  count        = "${var.setup_listener_rule && var.setup_target_group == false ? length(var.domains) : 0}"
   listener_arn = "${var.alb_listener_https_arn}"
   priority     = "${var.domain_priority_init + count.index}"
 
@@ -20,12 +40,36 @@ resource "aws_alb_listener_rule" "domain_https" {
     create_before_destroy = true
   }
 }
-
 #----------------------------------------
 # Domain & URL Mixed Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "domain_and_url_https" {
-  count        = "${var.setup_listener_rule ? length(var.domains_and_urls) : 0}"
+  count        = "${var.setup_listener_rule && var.setup_target_group ? length(var.domains_and_urls) : 0}"
+  listener_arn = "${var.alb_listener_https_arn}"
+  priority     = "${var.domain_and_url_priority_init + count.index}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_alb_target_group.app.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${element(keys(var.domains_and_urls), count.index)}"]
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["${element(values(var.domains_and_urls), count.index)}"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_alb_listener_rule" "domain_and_url_https" {
+  count        = "${var.setup_listener_rule && var.setup_target_group == false ? length(var.domains_and_urls) : 0}"
   listener_arn = "${var.alb_listener_https_arn}"
   priority     = "${var.domain_and_url_priority_init + count.index}"
 
@@ -48,12 +92,31 @@ resource "aws_alb_listener_rule" "domain_and_url_https" {
     create_before_destroy = true
   }
 }
-
 #----------------------------------------
 # URL Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "url_https" {
-  count        = "${var.setup_listener_rule ? length(var.urls) : 0}"
+  count        = "${var.setup_listener_rule && var.setup_target_group ? length(var.urls) : 0}"
+  listener_arn = "${var.alb_listener_https_arn}"
+  priority     = "${var.url_priority_init + count.index}"
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_alb_target_group.app.arn}"
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["${element(var.urls, count.index)}"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_alb_listener_rule" "url_https" {
+  count        = "${var.setup_listener_rule && var.setup_target_group == false ? length(var.urls) : 0}"
   listener_arn = "${var.alb_listener_https_arn}"
   priority     = "${var.url_priority_init + count.index}"
 
