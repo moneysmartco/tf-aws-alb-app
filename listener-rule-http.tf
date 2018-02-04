4c23129a455f5e6f6b30911aa0d11eb4ed89bc3e
Tung Nguyen <tungns.inf@gmail.com>
2018-02-04 18:13:19 +0800

    Maybe

--- listener-rule-http.tf
+++ listener-rule-http.tf
@@ -22,7 +22,7 @@ resource "aws_alb_listener_rule" "domain_http" {
 }
 
 resource "aws_alb_listener_rule" "domain_http_custom" {
-  count        = "${var.setup_listener_rule && var.setup_target_group == false ? length(var.domains) : 0}"
+  count        = "${var.setup_listener_rule && (var.setup_target_group == false) ? length(var.domains) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.domain_priority_init + count.index}"
 
@@ -69,7 +69,7 @@ resource "aws_alb_listener_rule" "domain_and_url_http" {
 }
 
 resource "aws_alb_listener_rule" "domain_and_url_http_custom" {
-  count        = "${var.setup_listener_rule && var.setup_target_group == false ? length(var.domains_and_urls) : 0}"
+  count        = "${var.setup_listener_rule && (var.setup_target_group == false) ? length(var.domains_and_urls) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.domain_and_url_priority_init + count.index}"
 
@@ -116,7 +116,7 @@ resource "aws_alb_listener_rule" "url_http" {
 }
 
 resource "aws_alb_listener_rule" "url_http_custom" {
-  count        = "${var.setup_listener_rule && var.setup_target_group == false ? length(var.urls) : 0}"
+  count        = "${var.setup_listener_rule && (var.setup_target_group == false) ? length(var.urls) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.url_priority_init + count.index}"
 
