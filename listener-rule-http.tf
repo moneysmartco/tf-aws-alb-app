108cbeb3b0f456248e6d07b66cb71ffc9130a830
Tung Nguyen <tungns.inf@gmail.com>
2018-02-04 17:35:24 +0800

    Update var name

--- listener-rule-http.tf
+++ listener-rule-http.tf
@@ -2,7 +2,7 @@
 # Domain Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "domain_http" {
-  count        = "${var.setup ? length(var.domains) : 0}"
+  count        = "${var.setup_listener_rule ? length(var.domains) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.domain_priority_init + count.index}"
 
@@ -25,7 +25,7 @@ resource "aws_alb_listener_rule" "domain_http" {
 # Domain & URL Mixed Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "domain_and_url_http" {
-  count        = "${var.setup ? length(var.domains_and_urls) : 0}"
+  count        = "${var.setup_listener_rule ? length(var.domains_and_urls) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.domain_and_url_priority_init + count.index}"
 
@@ -53,7 +53,7 @@ resource "aws_alb_listener_rule" "domain_and_url_http" {
 # URL Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "url_http" {
-  count        = "${var.setup ? length(var.urls) : 0}"
+  count        = "${var.setup_listener_rule ? length(var.urls) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.url_priority_init + count.index}"
 
--- listener-rule-https.tf
+++ listener-rule-https.tf
@@ -2,7 +2,7 @@
 # Domain Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "domain_https" {
-  count        = "${var.setup ? length(var.domains) : 0}"
+  count        = "${var.setup_listener_rule ? length(var.domains) : 0}"
   listener_arn = "${var.alb_listener_https_arn}"
   priority     = "${var.domain_priority_init + count.index}"
 
@@ -25,7 +25,7 @@ resource "aws_alb_listener_rule" "domain_https" {
 # Domain & URL Mixed Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "domain_and_url_https" {
-  count        = "${var.setup ? length(var.domains_and_urls) : 0}"
+  count        = "${var.setup_listener_rule ? length(var.domains_and_urls) : 0}"
   listener_arn = "${var.alb_listener_https_arn}"
   priority     = "${var.domain_and_url_priority_init + count.index}"
 
@@ -53,7 +53,7 @@ resource "aws_alb_listener_rule" "domain_and_url_https" {
 # URL Config
 #----------------------------------------
 resource "aws_alb_listener_rule" "url_https" {
-  count        = "${var.setup ? length(var.urls) : 0}"
+  count        = "${var.setup_listener_rule ? length(var.urls) : 0}"
   listener_arn = "${var.alb_listener_https_arn}"
   priority     = "${var.url_priority_init + count.index}"
 
