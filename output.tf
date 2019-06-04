466b31fa6af72e54a5a9b5b81fe20a6dc007ed6b
Brian <tjtbrian@gmail.com>
2019-06-04 17:46:21 +0800

    add target group arn_suffix

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "target_group_arn" {
 output "alb_listener_rule_domain_https_arn" {
   value = "${aws_alb_listener_rule.domain_https.*.arn}"
 }
+
+output "alb_target_group_arn_suffix" {
+  value = "${aws_alb_target_group.app.arn_suffix}"
+}
