0d4a5a0c4770ffb6e7198e9b7e21ccf175f9bd09
Eric Ho <dho.eric@gmail.com>
2019-06-07 19:32:32 +0800

    Apply on http

--- listener-rule-http.tf
+++ listener-rule-http.tf
@@ -3,7 +3,7 @@
 #----------------------------------------
 // Target group created inside module
 resource "aws_alb_listener_rule" "domain_http" {
-  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group ? length(var.domains) : 0}"
+  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group ? ceil(length(var.domains)/5.0) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.domain_priority_init + count.index}"
 
@@ -14,7 +14,10 @@ resource "aws_alb_listener_rule" "domain_http" {
 
   condition {
     field  = "host-header"
-    values = ["${element(var.domains, count.index)}"]
+
+    host_header {
+      values = ["${slice(var.domains, count.index*5, min(length(var.domains), (count.index+1)*5))}"]
+    }
   }
 
   lifecycle {
@@ -24,8 +27,7 @@ resource "aws_alb_listener_rule" "domain_http" {
 
 // Target group passed from caller
 resource "aws_alb_listener_rule" "domain_http_custom" {
-  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group == 0 ? length(var.domains) : 0}"
-
+  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group == 0 ? ceil(length(var.domains)/5.0) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.domain_priority_init + count.index}"
 
@@ -36,7 +38,10 @@ resource "aws_alb_listener_rule" "domain_http_custom" {
 
   condition {
     field  = "host-header"
-    values = ["${element(var.domains, count.index)}"]
+
+    host_header {
+      values = ["${slice(var.domains, count.index*5, min(length(var.domains), (count.index+1)*5))}"]
+    }
   }
 
   lifecycle {
@@ -49,7 +54,7 @@ resource "aws_alb_listener_rule" "domain_http_custom" {
 #----------------------------------------
 // Target group created inside module
 resource "aws_alb_listener_rule" "cognito_domain_http" {
-  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group ? length(var.cognito_domains) : 0}"
+  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group ? ceil(length(var.cognito_domains)/5.0) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.cognito_domain_priority_init + count.index}"
 
@@ -70,7 +75,10 @@ resource "aws_alb_listener_rule" "cognito_domain_http" {
 
   condition {
     field  = "host-header"
-    values = ["${element(var.cognito_domains, count.index)}"]
+
+    host_header {
+      values = ["${slice(var.cognito_domains, count.index*5, min(length(var.cognito_domains), (count.index+1)*5))}"]
+    }
   }
 
   lifecycle {
@@ -80,8 +88,7 @@ resource "aws_alb_listener_rule" "cognito_domain_http" {
 
 // Target group passed from caller
 resource "aws_alb_listener_rule" "cognito_domain_http_custom" {
-  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group == 0 ? length(var.cognito_domains) : 0}"
-
+  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group == 0 ? ceil(length(var.cognito_domains)/5.0) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.cognito_domain_priority_init + count.index}"
 
@@ -102,13 +109,18 @@ resource "aws_alb_listener_rule" "cognito_domain_http_custom" {
 
   condition {
     field  = "host-header"
-    values = ["${element(var.cognito_domains, count.index)}"]
+
+    host_header {
+      values = ["${slice(var.cognito_domains, count.index*5, min(length(var.cognito_domains), (count.index+1)*5))}"]
+    }
   }
 
   lifecycle {
     create_before_destroy = true
   }
 }
+
+
 #----------------------------------------
 # Domain & URL Mixed Config
 # This resource will only cater for a unqiue host-header with one or more path-pattern
@@ -242,7 +254,7 @@ resource "aws_alb_listener_rule" "cognito_domain_and_url_http_custom" {
 #----------------------------------------
 // Target group created inside module
 resource "aws_alb_listener_rule" "url_http" {
-  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group ? length(var.urls) : 0}"
+  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group ? ceil(length(var.urls)/5.0) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.url_priority_init + count.index}"
 
@@ -253,7 +265,10 @@ resource "aws_alb_listener_rule" "url_http" {
 
   condition {
     field  = "path-pattern"
-    values = ["${element(var.urls, count.index)}"]
+
+    path_pattern {
+      values = ["${slice(var.urls, count.index*5, min(length(var.urls), (count.index+1)*5))}"]
+    }
   }
 
   lifecycle {
@@ -263,7 +278,7 @@ resource "aws_alb_listener_rule" "url_http" {
 
 // Target group passed from caller
 resource "aws_alb_listener_rule" "url_http_custom" {
-  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group == 0 ? length(var.urls) : 0}"
+  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group == 0 ? ceil(length(var.urls)/5.0) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.url_priority_init + count.index}"
 
@@ -274,7 +289,10 @@ resource "aws_alb_listener_rule" "url_http_custom" {
 
   condition {
     field  = "path-pattern"
-    values = ["${element(var.urls, count.index)}"]
+
+    path_pattern {
+      values = ["${slice(var.urls, count.index*5, min(length(var.urls), (count.index+1)*5))}"]
+    }
   }
 
   lifecycle {
@@ -287,7 +305,7 @@ resource "aws_alb_listener_rule" "url_http_custom" {
 #----------------------------------------
 // Target group created inside module
 resource "aws_alb_listener_rule" "cognito_url_http" {
-  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group ? length(var.cognito_urls) : 0}"
+  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group ? ceil(length(var.cognito_urls)/5.0) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.cognito_url_priority_init + count.index}"
 
@@ -308,7 +326,10 @@ resource "aws_alb_listener_rule" "cognito_url_http" {
 
   condition {
     field  = "path-pattern"
-    values = ["${element(var.cognito_urls, count.index)}"]
+
+    path_pattern {
+      values = ["${slice(var.cognito_urls, count.index*5, min(length(var.cognito_urls), (count.index+1)*5))}"]
+    }
   }
 
   lifecycle {
@@ -318,7 +339,7 @@ resource "aws_alb_listener_rule" "cognito_url_http" {
 
 // Target group passed from caller
 resource "aws_alb_listener_rule" "cognito_url_http_custom" {
-  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group == 0 ? length(var.cognito_urls) : 0}"
+  count        = "${var.setup_listener_rule && var.enable_http_rules && var.setup_target_group == 0 ? ceil(length(var.cognito_urls)/5.0) : 0}"
   listener_arn = "${var.alb_listener_http_arn}"
   priority     = "${var.cognito_url_priority_init + count.index}"
 
@@ -339,7 +360,10 @@ resource "aws_alb_listener_rule" "cognito_url_http_custom" {
 
   condition {
     field  = "path-pattern"
-    values = ["${element(var.cognito_urls, count.index)}"]
+
+    path_pattern {
+      values = ["${slice(var.cognito_urls, count.index*5, min(length(var.cognito_urls), (count.index+1)*5))}"]
+    }
   }
 
   lifecycle {
