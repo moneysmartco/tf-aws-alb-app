c471f0fbb1f64ec1f83aeaf9bc77fbd07ba969df
petercipriano-ms <105843078+petercipriano-ms@users.noreply.github.com>
2023-08-08 10:24:35 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_alb_target_group" "app" {
   # name_prefix = "${var.env != "" ? format("%s-%s", var.app_name, var.env) : var.app_name}"
   # Target group name is 32 characters max
   name = replace(
-    var.env != "" ? format("%s-%s-%s", var.app_name, var.env, var.eks) : var.app_name,
+    var.env != "" ? format("%s-%s", var.eks, var.app_name,) : var.app_name,
     "/(.{0,32})(.*)/",
     "$1",
   )
